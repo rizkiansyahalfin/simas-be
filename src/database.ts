e343588987e9530b5ee9882b4e690283982af075
DsuRhan <shadow191006@gmail.com>
2026-05-09 09:00:32 +0700

    Change import

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/client.js'
+import { PrismaClient } from './generated/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 import dotenv from 'dotenv'
