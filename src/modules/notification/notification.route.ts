d552177f7d2c166a2a2906f3bfbc565968d9d73d
DsuRhan <shadow191006@gmail.com>
2026-05-22 10:12:26 +0700

    Add notification logic and trigger on donation, events, and inventory loan

--- src/modules/notification/notification.route.ts
+++ src/modules/notification/notification.route.ts
@@ -9,32 +9,77 @@ import {
   authMiddleware,
 } from '../../middlewares/auth.middleware'
 
+import {
+  rbacMiddleware,
+} from '../../middlewares/rbac.middleware'
+
+import {
+  Role,
+} from '../../generated/enums'
+
 const router = Router()
 
 router.use(authMiddleware)
 
 router.get(
   '/',
+
+  rbacMiddleware(
+    Role.superadmin,
+    Role.bendahara,
+    Role.admin_kegiatan,
+    Role.admin_inventaris,
+  ),
+
   notificationController.getNotifications
 )
 
 router.get(
   '/stream',
+
+  rbacMiddleware(
+    Role.superadmin,
+    Role.bendahara,
+    Role.admin_kegiatan,
+    Role.admin_inventaris,
+  ),
+
   notificationController.streamNotifications
 )
 
 router.patch(
   '/:id/read',
+
+  rbacMiddleware(
+    Role.superadmin,
+    Role.bendahara,
+    Role.admin_kegiatan,
+    Role.admin_inventaris,
+  ),
+
   notificationController.markNotificationRead
 )
 
 router.patch(
   '/read-all',
+
+  rbacMiddleware(
+    Role.superadmin,
+    Role.bendahara,
+    Role.admin_kegiatan,
+    Role.admin_inventaris,
+  ),
+
   notificationController.markAllNotificationsRead
 )
 
 router.delete(
   '/:id',
+
+  rbacMiddleware(
+    Role.superadmin,
+  ),
+
   notificationController.deleteNotification
 )
 
