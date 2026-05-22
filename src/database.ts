87cdb081633463eb38274e0bee830a1223daabf9
Rizkiansyah Alfin <rizkiansyahalfin030603@gmail.com>
2026-05-22 14:07:52 +0700

    feat: migrate to Prisma Postgres with consolidated schema

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,8 @@
-import { PrismaClient } from './generated/client'
+import { PrismaClient } from './generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
-import dotenv from 'dotenv'
-dotenv.config()
+import 'dotenv/config'
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-})
-
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 const prisma = new PrismaClient({ adapter })
 
--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import {  Role } from "../generated/enums";
+import { Role } from "../generated/prisma/enums";
 import prisma from "../database";
 
 
