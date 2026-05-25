c213594018bc0ec5d81305d4eb6e742c7cbec22f
DsuRhan <shadow191006@gmail.com>
2026-05-25 09:00:14 +0700

    Add regresh token to auth

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "src/prisma",
+  schema: "src/prisma/",
   migrations: {
     path: "src/prisma/migrations",
   },
