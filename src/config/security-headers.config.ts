5bd3f3a3c8be69050b0ded4d42229fc0438d7cf5
DsuRhan <shadow191006@gmail.com>
2026-06-11 14:03:03 +0700

    Init branch and add new security headers config

---
+++ src/config/security-headers.config.ts
@@ -0,0 +1,59 @@
+export const securityHeadersConfig = {
+
+  hsts: {
+    maxAge:
+      31536000,
+    includeSubDomains:
+      true,
+    preload:
+      true
+  },
+
+  contentSecurityPolicy: {
+
+    useDefaults: true,
+
+    directives: {
+
+      defaultSrc: [
+        "'self'"
+      ],
+
+      scriptSrc: [
+        "'self'"
+      ],
+
+      styleSrc: [
+        "'self'",
+        "'unsafe-inline'"
+      ],
+
+      imgSrc: [
+        "'self'",
+        "data:",
+        "blob:"
+      ],
+
+      connectSrc: [
+        "'self'",
+        "http://localhost:5173"
+      ],
+
+      objectSrc: [
+        "'none'"
+      ],
+
+      frameAncestors: [
+        "'none'"
+      ],
+
+      baseUri: [
+        "'self'"
+      ],
+
+      formAction: [
+        "'self'"
+      ]
+    }
+  }
+}
