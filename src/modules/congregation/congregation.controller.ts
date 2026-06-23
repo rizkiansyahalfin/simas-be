39268c485a7ef4b96bd82bad2d3c72b0748b4af5
DsuRhan <shadow191006@gmail.com>
2026-06-23 09:50:18 +0700

    resolve conflict

--- src/modules/congregation/congregation.controller.ts
+++ src/modules/congregation/congregation.controller.ts
@@ -108,20 +108,10 @@ export const CongregationController = {
         status: "success",
         message: "Congregation deleted successfully"
       })
-<<<<<<< HEAD
-  })
-}
-=======
-    } catch (err) {
-      next(err)
     }
-  },
-  async getQrCode(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
+  ),
+
+  getQrCode: asyncHandler(async (req: Request, res: Response) => {
     const id = Number(req.params.id)
 
     if (isNaN(id)) {
@@ -138,10 +128,5 @@ export const CongregationController = {
       status: "success",
       data: result
     })
-
-  } catch (err) {
-    next(err)
-  }
-},
+  })
 }
->>>>>>> feat/be-congregation-qr-code
